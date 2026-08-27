aed4a4cd9c3f02464089217528dabac8a6018e28
tcarruth <tomsinbox@gmail.com>
2026-08-27 15:16:29 -0700

    Sardine optimization

--- OMs/Sardine/Dev/dev code.R
+++ OMs/Sardine/Dev/dev code.R
@@ -46,3 +46,80 @@ opt = optim(par = c(0,log(max(R,na.rm=T)/2)), BH,
 BH(opt$par, B, R, plot=T)
 
 
+B = est_yr$B_west + est_yr$B_south # Biomass Mt (could be unitless, R0 and steepness h are the targets)
+R = est_yr$R_west + est_yr$R_south # Billions of fish
+
+ctrl = SimControl(DynamicUnfished = FALSE,
+                     RefLandings = FALSE,
+                     RefRemovals = FALSE,
+                     ConditionObs = FALSE,
+                     EstimateBeta = FALSE,
+                     GenerateData = FALSE,
+                     MSYRefs = FALSE,
+                     RefPoints = FALSE,
+                     MGT = FALSE,
+                     BLow = FALSE)
+
+
+Apply_pars = function(om, pars){
+  R0mult = exp(pars[1])
+  Emult = exp(-pars[1]) * exp(pars[2])                      # fried egg please!
+  om@Stock@SRR@R0 = om@Stock@SRR@R0 * R0mult
+  om@Fleet@Effort@Effort =  om@Fleet@Effort@Effort * Emult
+  om
+}
+
+
+doplot = function(om, B, R, Best, Rest, obj){
+  par(mfrow =c(1,2),mai=c(0.5,0.5,0.2,0.05))
+  ind = !is.na(R); yrs = Years(om, 'h')[ind]
+  matplot(yrs, cbind(R[ind],Rest[ind]),col=c("black","red"),lty=1,lwd=1.5,type = 'l');grid()
+  legend('topleft',legend=c("Assessment","OpenMSE"),text.col = c("black","red"), bty="n")
+  matplot(yrs,cbind(B[ind],Best[ind]),col=c("black","red"),lty=1,lwd=1.5,type = 'l');grid()
+  legend('topleft',legend=c(paste("R0 multiplier =",round(exp(pars[1]),3)),
+                            paste("Effort multiplier =",round(exp(pars[2]),3)),
+                            paste("Obj. F. =", round(obj,5))), bty="n")
+}  
+
+Int_Scale = function(pars, om, B, R, mode = 'opt', ctrl, plot=F){
+  om = Apply_pars(om, pars)
+  hist = Simulate(om, control = ctrl)
+  Best = hist@Biomass[1,1,] * 1E-9
+  Rest = hist@Number[[1]][1,1,,1] * 1E-9
+  if(mode == 'opt')  obj = sum(abs(log(Best)-log(B)),na.rm=T) + sum(abs(log(Rest)-log(R)),na.rm=T)
+  if(mode == 'optB')  obj = sum(abs(log(Best)-log(B)),na.rm=T)
+  if(plot) doplot(om, B, R, Best, Rest, obj)
+  if(grepl("opt", mode)){
+    return(obj)
+  }else{
+    pars
+  }
+} 
+
+opt = optim(c(0,0), Int_Scale, 
+      om = om, B = B, R = R, ctrl = ctrl, plot=T,
+      method = "L-BFGS-B", control = list(factr = 1E12),
+      lower = c(-2, -2), upper = c(2, 2))
+
+om2 = Apply_pars(om,opt$par)
+
+for(i in 1:2){
+  hist = Simulate(om2, control=control)
+  Best = hist@Biomass[1,1,] * 1E-9
+  Rest = hist@Number[[1]][1,1,,1] * 1E-9
+  doplot(om2, B, R, Best, Rest, obj = 0)
+  ratio = matrix(R/Rest,byrow=T,nrow=om@nSim,ncol=om@nYear)
+  ratio[is.na(ratio)] = 1
+  R0_adj = exp(log(ratio)*0.5)
+  om2@Stock@SRR@RecDevHist[] = om2@Stock@SRR@RecDevHist[] * R0_adj
+}
+
+opt = optim(c(0,0), Int_Scale, 
+            om = om2, B = B, R = R, ctrl = ctrl, mode = "optB", plot=T,
+            method = "L-BFGS-B", control = list(factr = 1E12),
+            lower = c(-2, -2), upper = c(2, 2))
+
+
+om3 = Apply_pars(om2,opt$par)
+
+
